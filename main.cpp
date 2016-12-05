511448851757b9c857bff2e2b42d3b2b137d2273
elisakis <elisa.kis@yandex.ru>
2016-12-05 22:01:32 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,46 @@
+//Задание 4 с массивами
+#include <iostream>
+
+using namespace std;
+
+void dig_trench ( int l, int Trench[6][6] )
+{
+    bool z;
+    int n;
+    for (int i = 0; i < l; i++)
+    {
+        n = i;
+        z = false;
+
+        for (int j = 0; j < l; j++)
+        {
+            if (n == 0)
+                z = true;
+
+            Trench[i][j] = n;
+
+            if (z)
+                n++;
+            else
+                n--;
+
+        }
+    }
+}
+
+int main()
+{
+    int l = 6;
+    int Trench[6][6];
+
+    dig_trench (l, Trench);
+
+    for (int i = 0; i < l; i++)
+    {
+        for (int j = 0; j < l; j++)
+        {
+            cout << Trench[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
